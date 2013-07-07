cdfe44d56335082108f59aef108a3d8544b5ae5d
saiprasanth.s <saiprasanths@saiprasanths-Inspiron-3420.(none)>
2013-07-07 11:21:33 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsite::Application.config.secret_token = 'df7b7a457560eb236ba0f154738f68ce290c8f75022419961919847315f362d14f0f112e0e960a351327de1460e280f9fbdc3e63730fef24c20df1bb98d2858f'
